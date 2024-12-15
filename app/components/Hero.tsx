430563b3ba8fdbfaab7170781f386f161f9f7869
abdulrehman1771 <hhatori476@gmail.com>
2024-12-15 12:31:11 +0500

    Hero Section Made Responsive

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,37 +3,49 @@ export default function Hero() {
   return (
     <>
       <div className="w-full bg-[#F2F0FF]">
-        <div className="flex w-[95%] mx-auto max-w-[1900px]">
-          <div className="relative">
-            {/* <img src="lamp-hero.png" alt="" /> */}
-            <Image src="/lamp-hero.png" width={20} height={20} className="relative top-0 scale-75" layout="responsive" alt=""></Image>
+        <div className="flex flex-col w-[95%] mx-auto max-w-[1900px]">
+          <div className="flex">
+            <div className="hidden sm:flex sm:flex-col">
+              <div>
+                <Image
+                  src="/lamp-hero.png"
+                  width={20}
+                  height={20}
+                  className=""
+                  layout="responsive"
+                  alt=""
+                ></Image>
+              </div>
+            </div>
+            <div className="py-10 self-center flex flex-col justify-center">
+              <div className="text-lg text-center md:text-left md:text-xl text-[#FB2E86] leading-[5rem]">
+                <p>Best Furniture For Your Castle....</p>
+              </div>
+              <div className=" max-w-[800px] josefin-sans text-center md:text-left text-3xl md:text-4xl lg:text-6xl font-bold leading-[2rem] md:leading-[3rem] lg:leading-[5rem]">
+                <h1>New Furniture Collection Trends in 2020</h1>
+              </div>
+              <div className="text-center md:text-left text-sm md:text-xl text-[#8A8FB9]">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna
+                  in est adipiscing in phasellus non in justo.
+                </p>
+              </div>
+              <div className="items-center self-center scale-75 md:scale-100 md:self-auto justify-center font-medium mt-8 text-xl text-white josefin-sans">
+                <button title="button" type="button" className=" px-9 py-3  bg-[#FB2E86]">Shop Now</button>
+              </div>
+            </div>
+            <div className="hidden sm:flex sm:items-center">
+              <Image
+                className="mt-5"
+                src="/sofa-hero.png"
+                width={20}
+                height={20}
+                layout="responsive"
+                alt=""
+              ></Image>
+            </div>
           </div>
-          <div className=" self-center">
-            <div className="text-xl text-[#FB2E86] leading-[5rem]">
-              <p>Best Furniture For Your Castle....</p>
-            </div>
-            <div className="max-w-[800px] josefin-sans text-6xl font-bold leading-[5rem]">
-              <h1>New Furniture Collection Trends in 2020</h1>
-            </div>
-            <div className="text-xl text-[#8A8FB9]">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna
-                in est adipiscing in phasellus non in justo.
-              </p>
-            </div>
-            <div className="items-center justify-center font-medium mt-8 text-xl text-white josefin-sans">
-              <button className=" px-9 py-3  bg-[#FB2E86]">Shop Now</button>
-            </div>
-          </div>
-          <div className="flex items-center">
-            
-              {/* <img className="w-[629px]" src="sofa-hero.png" alt="" /> */}
-            <Image className="mt-5" src="/sofa-hero.png" width={20} height={20} layout="responsive" alt=""></Image>
-            
-          </div>
-        </div>
 
-        
           <div className="flex gap-16 items-center justify-center scale-[20%] pb-9">
             {/* Green diamond with pink center */}
             <div className="relative w-12 h-12 bg-transparent border-4 border-pink-500 rotate-45 flex items-center justify-center">
@@ -44,7 +56,7 @@ export default function Hero() {
             <div className="w-12 h-12 bg-transparent border-4 border-pink-500 rotate-45"></div>
             <div className="w-12 h-12 bg-transparent border-4 border-pink-500 rotate-45"></div>
           </div>
-        
+        </div>
       </div>
     </>
   );
